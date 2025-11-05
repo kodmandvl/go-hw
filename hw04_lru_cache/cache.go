65edb2ffb925fd87ddafda48a0253fad7845693f
kodmandvl <kodmandvl@mail.ru>
2025-11-05 13:20:00 +0300

    HW4 is completed

--- hw04_lru_cache/cache.go
+++ hw04_lru_cache/cache.go
@@ -1,5 +1,9 @@
 package hw04lrucache
 
+import (
+	"sync"
+)
+
 type Key string
 
 type Cache interface {
@@ -9,11 +13,10 @@ type Cache interface {
 }
 
 type lruCache struct {
-	Cache // Remove me after realization.
-
 	capacity int
 	queue    List
 	items    map[Key]*ListItem
+	lock     *sync.Mutex
 }
 
 func NewCache(capacity int) Cache {
@@ -21,5 +24,76 @@ func NewCache(capacity int) Cache {
 		capacity: capacity,
 		queue:    NewList(),
 		items:    make(map[Key]*ListItem, capacity),
+		lock:     new(sync.Mutex),
 	}
+}
+
+type cacheElem struct {
+	k Key
+	v interface{}
+}
+
+// Чтобы сделать кэш горутино-безопасным, добавим sync.Mutex в структуру кэша
+// и вызов Lock в начале функций, а Unlock - в конце, через defer.
+// Это позволит защитить кэш, т.к. Mutex занять может только одна горутина.
+// В таком случае наш кэш будет уже каким-то подобием in-memory БД с защищенным доступом
+
+// Set(key Key, value interface{}) bool // Добавить значение в кэш по ключу.
+func (c *lruCache) Set(key Key, value interface{}) bool {
+	// В начале ставим блокировку (mutex):
+	c.lock.Lock()
+	// В конце освобождаем блокировку (mutex):
+	defer c.lock.Unlock()
+	// Пытаемся получить элемент по ключу:
+	elem, ok := c.items[key]
+	if ok {
+		// Если нашли ключ, то переприсваиваем значение и перемещаем наверх:
+		elem.Value = &cacheElem{
+			k: key,
+			v: value,
+		}
+		c.queue.MoveToFront(elem)
+		return true // ключ был, обновили имеющийся элемнт в кэше
+	}
+	// Если не нашли ключ, то добавляем новый элемент в кэш:
+	newElem := c.queue.PushFront(&cacheElem{
+		k: key,
+		v: value,
+	})
+	c.items[key] = newElem
+	// При этом если превысили ёмкость кэша, то удаляем последний элемент:
+	if c.queue.Len() > c.capacity {
+		lastElem := c.queue.Back()
+		c.queue.Remove(lastElem)                       // удаляем из списка
+		delete(c.items, lastElem.Value.(*cacheElem).k) // удаляем из мапы
+	}
+	return false // ключа не было, добавили новый элемент в кэш
+}
+
+// Get(key Key) (interface{}, bool) // Получить значение из кэша по ключу.
+func (c *lruCache) Get(key Key) (interface{}, bool) {
+	// В начале ставим блокировку (mutex):
+	c.lock.Lock()
+	// В конце освобождаем блокировку (mutex):
+	defer c.lock.Unlock()
+	// Пытаемся получить элемент по ключу:
+	elem, ok := c.items[key]
+	if ok {
+		// Если нашли ключ, то перемещаем его наверх, а также возвращаем значение и true:
+		c.queue.MoveToFront(elem)
+		return elem.Value.(*cacheElem).v, true
+	}
+	// Если не нашли ключ, то возвращаем nil и false:
+	return nil, false
+}
+
+// Clear() // Очистить кэш.
+func (c *lruCache) Clear() {
+	// В начале ставим блокировку (mutex):
+	c.lock.Lock()
+	// В конце освобождаем блокировку (mutex):
+	defer c.lock.Unlock()
+	// Очищаем кэш (приводим к состоянию нового пустого кэша с текущей capacity):
+	c.items = make(map[Key]*ListItem, c.capacity) // новая чистая мапа, но с текущей capacity (см. func NewCache)
+	c.queue = NewList()                           // новый пустой список (см. func NewCache)
 }
--- hw04_lru_cache/list.go
+++ hw04_lru_cache/list.go
@@ -17,10 +17,138 @@ type ListItem struct {
 }
 
 type list struct {
-	List // Remove me after realization.
-	// Place your code here.
+	length    int
+	listBegin *ListItem
+	listEnd   *ListItem
 }
 
 func NewList() List {
 	return new(list)
+}
+
+// Len() int // длина списка.
+func (l *list) Len() int {
+	return l.length
+}
+
+// Front() *ListItem // первый элемент списка.
+func (l *list) Front() *ListItem {
+	return l.listBegin
+}
+
+// Back() *ListItem // последний элемент списка.
+func (l *list) Back() *ListItem {
+	return l.listEnd
+}
+
+// PushFront(v interface{}) *ListItem // добавить значение в начало.
+func (l *list) PushFront(v interface{}) *ListItem {
+	// Новый элемент списка:
+	newElem := &ListItem{
+		Value: v,
+		Next:  l.listBegin,
+		Prev:  nil,
+	}
+
+	// Если l.listBegin != nil (т.е. если по сути список был не пуст),
+	// то теперь у бывшего "начала списка" предыдущий элемент теперь - newElem:
+	if l.listBegin != nil {
+		l.listBegin.Prev = newElem
+	} else {
+		// Иначе (т.е. если спиок был пуст) теперь это будет последний элемент:
+		l.listEnd = newElem
+	}
+
+	// При этом добавляемый элемент также будет первым в списке:
+	l.listBegin = newElem
+
+	// Длина списка увеличивается на 1:
+	l.length++
+
+	// Теперь после всех необходимых проверок и действий возвращаем этот новый элемент:
+	return newElem
+}
+
+// PushBack(v interface{}) *ListItem // добавить значение в конец.
+func (l *list) PushBack(v interface{}) *ListItem {
+	// Новый элемент списка:
+	newElem := &ListItem{
+		Value: v,
+		Next:  nil,
+		Prev:  l.listEnd,
+	}
+
+	// Если l.listEnd != nil (т.е. если по сути список был не пуст),
+	// то теперь у бывшего "конца списка" следующий элемент - newElem:
+	if l.listEnd != nil {
+		l.listEnd.Next = newElem
+	} else {
+		// Иначе (т.е. если спиок был пуст) теперь это будет первый элемент:
+		l.listBegin = newElem
+	}
+
+	// При этом добавляемый элемент также будет последним в списке:
+	l.listEnd = newElem
+
+	// Длина списка увеличивается на 1:
+	l.length++
+
+	// Теперь после всех необходимых проверок и действий возвращаем этот новый элемент:
+	return newElem
+}
+
+// Remove(i *ListItem) // удалить элемент.
+func (l *list) Remove(i *ListItem) {
+	// Если предыдущего элемента нет, значит этот был первым в списке:
+	if i.Prev == nil {
+		// Тогда теперь следующий элемент будет первым:
+		l.listBegin = i.Next
+		// и у него не будет предыдущего элемента:
+		if l.listBegin != nil {
+			l.listBegin.Prev = nil
+		}
+	} else {
+		// Иначе следующий у предыдущего - это следующий у удаляемого
+		// (убрали удаляемый элемент из цепочки):
+		i.Prev.Next = i.Next
+	}
+
+	// Если следующего элемента нет, значит этот был последним в списке:
+	if i.Next == nil {
+		// Тогда теперь предыдущий элемент будет последним:
+		l.listEnd = i.Prev
+		// и у него не будет следующего элемента:
+		if l.listEnd != nil {
+			l.listEnd.Next = nil
+		}
+	} else {
+		// Иначе предыдущий у следующего - это предыдущий у удаляемого
+		// (убрали удаляемый элемент из цепочки):
+		i.Next.Prev = i.Prev
+	}
+
+	// Не забываем уменьшить длину списка:
+	l.length--
+}
+
+// MoveToFront(i *ListItem) // переместить элемент в начало.
+func (l *list) MoveToFront(i *ListItem) {
+	// Если этот элемент - и так начало списка, ничего делать не нужно:
+	if i == l.listBegin {
+		return
+	}
+
+	// Убираем перемещаемый элемент из цепочки:
+	i.Prev.Next = i.Next
+	if i.Next != nil {
+		i.Next.Prev = i.Prev
+	} else {
+		l.listEnd = i.Prev
+	}
+
+	// Размещаем его в начале и связываем с бывшим начальным элементом:
+	l.listBegin.Prev = i
+	i.Prev = nil
+	i.Next = l.listBegin
+	l.listBegin = i
 }
